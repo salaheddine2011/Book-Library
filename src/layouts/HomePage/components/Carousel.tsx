88c0a48390666c38b725d8e9745b69b2ba9d93e2
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-03-04 18:31:40 +0100

    Writing fetchBooks function inside useEffect

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -9,7 +9,34 @@ export const Carousel = () => {
 
     useEffect(() => {
         const fetchBooks = async () => {
+            const basedUrl: String = "http://localhost:8080/api";
+            const url: String = `${basedUrl}?page=0$size=9`;
+            const response = await fetch(url);
 
+            if (!response.ok) {
+                throw new Error("Something Went Wrong");
+            }
+
+            const responseJson = await response.json();
+            const responseData = responseJson._embeded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img,
+                });
+            }
+
+            setBooks(loadedBooks);
+            setIsLoading(false);
 
         };
         fetchBooks().catch((error: any) => {
