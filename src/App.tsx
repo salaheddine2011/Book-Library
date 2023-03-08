b230f12516c3ceb16e48d4163a0a5f375e56ea30
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-03-08 15:01:01 +0100

    adding SearchBook Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import './App.css';
 
 import { HomePage } from './layouts/HomePage/HomePage';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
 
-export const App=()=>{
+export const App = () => {
   return (
-   <HomePage/>
+    <div>
+      <Navbar />
+      {/* <HomePage /> */}
+      <SearchBooksPage/>
+      <Footer />
+    </div>
+
   );
 }
 
