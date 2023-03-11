d751bce20f8b6fbaed1751c1fd1b78a18d49ffa8
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-03-11 23:46:43 +0100

    initial chackout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 
 import { HomePage } from './layouts/HomePage/HomePage';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
@@ -22,6 +23,9 @@ export const App = () => {
           </Route>
           <Route path='/search'>
             <SearchBooksPage />
+          </Route>
+          <Route path='/checkout/:bookId'>
+                   <BookCheckoutPage/>
           </Route>
         </Switch>
       </div>
