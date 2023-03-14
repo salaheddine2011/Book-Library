edf165fbdad347cdf350804a2b556b061482e6dd
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-03-14 11:13:57 +0100

    Configuring Okta

---
+++ src/lib/oktaConfig.ts
@@ -0,0 +1,11 @@
+import { profile } from "console"
+import { Redirect } from "react-router-dom"
+
+export const oktaConfig = {
+    clientId: '0oa8pain8en7qSXnX5d7',
+    issuer: 'http://dev-12367658.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid','profile','email'],
+    pkce:true,
+    disableHttpsChecks:true,
+}
