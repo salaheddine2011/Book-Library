24d7d497714bf155db7693885d24be899c064b19
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-10-21 02:47:00 +0200

    adding requestResponse for admin

--- src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -1,5 +1,6 @@
 import { useOktaAuth } from "@okta/okta-react"
 import { useEffect, useState } from "react"
+import AdminMessageRequest from "../../../models/AdminMessageRequest";
 import MessageModel from "../../../models/MessageModel";
 import { Pagination } from "../../Utils/Pagination";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
@@ -21,6 +22,8 @@ export const AdminMessages = () => {
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(0)
     console.log(authState)
+    // Recall useEffect 
+    const [btnSubmit, setBtnSubmit] = useState(false)
 
     useEffect(() => {
         const fetchUserMessages = async () => {
@@ -55,7 +58,7 @@ export const AdminMessages = () => {
             setHttpError(error.message)
         })
         window.scrollTo(0, 0)
-    }, [authState, currentPage]);
+    }, [authState, currentPage, btnSubmit]);
 
     if (isLoadingMessages) {
         return <SpinnerLoading />
@@ -67,19 +70,42 @@ export const AdminMessages = () => {
             </div>
         )
     }
+
+    async function submitResponseToQuestion(id: number, response: string) {
+        const url = 'http://localhost:8080/api/messages/secure/admin/message';
+        if (authState && authState.isAuthenticated) {
+            const messageAdminRequestModel: AdminMessageRequest = new AdminMessageRequest(id, response);
+            const requestOptions = {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                    'Content-type': 'text/json'
+                },
+                Body : JSON.stringify(messageAdminRequestModel)
+
+            }
+            const messageAdminRequestModelResponse=await fetch(url,requestOptions)
+            if(!messageAdminRequestModel.response){
+                throw new Error('Something went wrong')
+            }
+            setBtnSubmit(!btnSubmit)
+
+        }
+    }
+
     const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
 
     return (
         <div className="mt-3">
             {messages.length > 0 ?
                 <>
-                {console.log(messages.length)}
+                    {console.log(messages.length)}
                     <h5>Pending Q/A</h5>
-                    {messages.map(message => ( 
+                    {messages.map(message => (
                         <AdminMessage message={message} key={message.id} />
                     ))}
                 </> :
-                <h5>No pending Q/A</h5> 
+                <h5>No pending Q/A</h5>
             }
             {totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />}
         </div>
