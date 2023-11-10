f0afbc1d31d1d42d12c1fb7ab476fe9198c4b550
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-11-10 13:35:48 +0100

    Configure index.tsx and index.html for stripe Payment Process

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+
+const stripePromise=loadStripe('pk_test_51OAh7bHE61o1l2tRUi38tNCzzSrylheSw2ARWVNiNaFJTaUlbamKSm9YkW03SGBmU17O0FAWeLtyApRkNOHj5XVS00peBFqkLH') // publishable key
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +14,10 @@ const root = ReactDOM.createRoot(
 root.render(
 
   <BrowserRouter>
-     <App />
+  <Elements stripe={stripePromise}>
+  <App />
+
+  </Elements>
   </BrowserRouter>
    
   
